bb8fb8df2bb132118738a54b6cb8eccbb141c1f7
FallenWarrior2k <mail@fallenwarrior.me>
2021-05-31 20:53:57 +0200

    Ensure consistent punctuation in error messages
    I really don't like it when statements don't end with a period.

--- src/main.rs
+++ src/main.rs
@@ -38,31 +38,31 @@ fn main() -> anyhow::Result<()> {
 
     let bg_atom = c
         .intern_atom(true, b"_XROOTPMAP_ID")
-        .context("Failed to create cookie to retrieve background atom ID")?
+        .context("Failed to create cookie to retrieve background atom ID.")?
         .reply()
-        .context("Failed to get background atom ID")?
+        .context("Failed to get background atom ID.")?
         .atom;
 
     let prop = c
         .get_property(false, root, bg_atom, AtomEnum::PIXMAP, 0, 1)
-        .context("Failed to create cookie to get background pixmap")?
+        .context("Failed to create cookie to get background pixmap.")?
         .reply()
-        .context("Failed to get background pixmap")?;
+        .context("Failed to get background pixmap.")?;
 
     // This is what Polybar does and it works
     let mut value_iter = prop
         .value32()
-        .with_context(|| format!("Unexpected pixmap reply format {}", prop.format))?;
-    let pixmap: Pixmap = value_iter.next().context("No background pixmap set")?;
+        .with_context(|| format!("Unexpected pixmap reply format {}.", prop.format))?;
+    let pixmap: Pixmap = value_iter.next().context("No background pixmap set.")?;
     if value_iter.next() != None {
-        bail!("Too many values in pixmap reply");
+        bail!("Too many values in pixmap reply.");
     }
 
     let geometry = c
         .get_geometry(pixmap)
-        .context("Failed to create cookie to retrieve background geometry")?
+        .context("Failed to create cookie to retrieve background geometry.")?
         .reply()
-        .context("Failed to grab background geometry")?;
+        .context("Failed to grab background geometry.")?;
 
     let image = c
         .get_image(
@@ -74,12 +74,12 @@ fn main() -> anyhow::Result<()> {
             geometry.height,
             !0, // All planes; X doesn't about extra bits
         )
-        .context("Failed to create cookie to retrieve background contents")?
+        .context("Failed to create cookie to retrieve background contents.")?
         .reply()
-        .context("Failed to grab background contents")?;
+        .context("Failed to grab background contents.")?;
 
     let raw_image = BgraImage::from_raw(geometry.width.into(), geometry.height.into(), image.data)
-        .context("Failed to create image")?;
+        .context("Failed to create image.")?;
 
     let image = match image.depth {
         // I haven't actually tested this; it's just conjecture from 24-bit being BGR0
@@ -91,7 +91,7 @@ fn main() -> anyhow::Result<()> {
         RGB_DEPTH => DynamicImage::ImageRgb8(map_pixels(&raw_image, |_, _, Bgra([b, g, r, _])| {
             Rgb([r, g, b])
         })),
-        depth => bail!("Unsupported pixel depth: {}", depth),
+        depth => bail!("Unsupported pixel depth {}.", depth),
     };
 
     if out_file == OsStr::new("-") {
@@ -100,9 +100,9 @@ fn main() -> anyhow::Result<()> {
                 &mut stdout(),
                 ImageOutputFormat::Pnm(PNMSubtype::ArbitraryMap),
             )
-            .context("Failed to write image")?;
+            .context("Failed to write image.")?;
     } else {
-        image.save(out_file).context("Failed to save image")?;
+        image.save(out_file).context("Failed to save image.")?;
     }
 
     Ok(())
